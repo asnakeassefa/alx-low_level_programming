52b58dd3a8c0d5467fce769ccf6c8121948c2020
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-10 22:23:37 +0300

    added

---
+++ 0-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+/**
+ * main - Entry point
+ * 
+ * REturn: Always 0 (Success)
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
