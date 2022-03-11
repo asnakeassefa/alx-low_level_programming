465956a07a7eef506c96392c36b47a4d74495994
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-11 23:16:24 +0300

    next c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 48;
+
+while (a < 58)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+	return (0);
+}
