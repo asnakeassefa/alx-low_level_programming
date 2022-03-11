9f262f27cb64f942c49d1a9a1c8e03a8ef188f08
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-12 00:43:01 +0300

    next c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 48;
+
+while (a < 58)
+{
+int b = 48;
+while (b < 57)
+{
+int c = a;
+while (c < 58)
+{
+int d = b + 1;
+while (d < 58)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if(a < 58)
+{
+if(a == 57 && b == 56 && c == 57 && d == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+d++;
+}
+c++;
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+	return (0);
+}
