2ad5187f8210248874ac856993eca8da5dfe04d9
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-11 23:51:30 +0300

    next c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 48;
+
+while (a < 57)
+{
+int b = a+1;
+
+while (b < 58)
+{
+putchar(a);
+putchar(b);
+if (a < 58)
+{
+putchar(',');
+if (a < 58)
+putchar(' ');
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+	return (0);
+}
