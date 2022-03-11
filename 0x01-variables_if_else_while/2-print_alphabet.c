63252b170a62b4267f192b79f2fe9f834194bdeb
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-11 22:37:46 +0300

    next c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{   
+    int a = 97;
+    while(a < 123){
+    putchar(a);
+    a++;
+    }
+	return (0);
+}
