262ba48363f6451988efdd391f8fd72f4baa9e6e
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-22 00:09:13 +0300

    My comm

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * @n: - First operand
+ * Nothing to return
+ */
+void swap_int(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
