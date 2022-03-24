8f3c5dc021e9aa4e87b13d873a7750474741586b
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-24 21:12:28 +0300

    added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,5 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        char *start = dest;
-
-        while (*dest != '\0')
-        {
-                dest++;
-        }
-        while (*src != '\0')
-        {
-                *dest = *src;
-                dest++;
-                src++;
-        }
-        *dest = '\0';
-        return (start);
+       
 }
