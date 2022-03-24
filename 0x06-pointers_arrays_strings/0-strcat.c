76929cf56158c91da49306bf6e7b7cfee1321f65
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-24 14:10:31 +0300

    added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,12 @@
-char *_strcat(char *dest, char *src){
-    char * str = strcat(*dest,*src);
-    return *str;
+/**
+ * @brief this is function
+ * 
+ * @param dest 
+ * @param src 
+ * @return char* 
+ */
+char *_strcat(char *dest, char *src)
+{
+char * str = strcat(*dest,*src);
+return *str;
 }
