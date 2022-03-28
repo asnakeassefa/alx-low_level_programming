ce62417df44d9b8c741736dc890e268182cb824d
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-28 18:46:25 +0300

    adde

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    for(int i=0; i<n; i++)
-        *(s + i) = b;
+unsigned int i;
 
-    return(s);
+for(i=0; i<n; i++)
+*(s + i) = b;
+return(s);
 }
