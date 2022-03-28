9e594b4251b32a9d1e89879f9554b1e47a43308f
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-28 18:58:38 +0300

    added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
-#include"main.h"
+#include "main.h"
+
 /**
  * _memset - is a funciton
- * @param s 
- * @param b 
- * @param n 
- * @return char*
+ * @s: this is s
+ * @b: this is b
+ * @n: this is n
+ * @Return: char*
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 
-for(i=0; i<n; i++)
+for (i = 0; i < n; i++)
 *(s + i) = b;
-return(s);
+return (s);
 }
