a3d8bf67a811e5244495f3d37868dc473973518e
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-28 20:28:41 +0300

    added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,23 @@
-#include "main.h" 
-
+#include "main.h"
 /**
- * _memset - is a funciton
- * @s: this is s
- * @b: this is b
- * @n: this is n
- * @Return: char*
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
 
-for (i = 0; i < n; i++)
-*(s + i) = b;
-return (s);
+{
+	unsigned int i;
+
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+
+	} /*END FOR*/
+
+	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,25 @@
-#include "main.h" 
-
+#include "main.h"
 /**
- * _memcpy - is a funciton
- * @dest: this is s
- * @src: this is b
- * @n: this is n
- * @Return: char*
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
-unsigned int i;
-for(i = 0; i < n; i++)
-*(dest + i) = *(src + i);
-return(dest);
+	unsigned int i;
+
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+
+	} /*END WHILE*/
+
+
+	return (dest);
 }
