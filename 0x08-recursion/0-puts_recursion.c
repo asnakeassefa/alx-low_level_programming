65264bdcf9abffbede0868286d389a2a985da650
asnakeassefa <asnakeassefa28@gmail.com>
2022-03-30 16:02:05 +0300

    added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * _puts_recursion - done recursion
+ * @s: this is parameter 
+ * Returns: none
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(s + _puts_recursion(s++));
+}
