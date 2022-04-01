6eae0c1b0d06ecb6527d281154d77c510d5e2581
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-01 17:57:26 +0300

    adde

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - is main function
+ *
+ * @argc: para
+ * @argv: para
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(argc);
+	return (0);
+}
