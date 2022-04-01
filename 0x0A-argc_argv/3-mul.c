1890c6f265b67ab646ba668ce4fdda2f464d4d02
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-01 19:52:04 +0300

    added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (0);
 	}
-	mul = atoi(argv[argc -1]) * atoi(argv[argc - 2]);
+	mul = atoi(argv[argc - 1]) * atoi(argv[argc - 2]);
 	printf("%d\n", mul);
 	return (0);
 }
