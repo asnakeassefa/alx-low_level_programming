e941ae763cd07c24fd81903f39f1f9de5a771d06
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-01 21:27:36 +0300

    added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,27 +8,27 @@
  * check_num - check - string there are digit
  * @str: array str
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int check_num(char *str)
 
 {
-/*Declaring variables*/
-unsigned int count;
+	/*Declaring variables*/
+	unsigned int count;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
+	count = 0;
+	while (count < strlen(str)) /*count string*/
 
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
 
-count++;
-}
-return (1);
+		count++;
+	}
+	return (1);
 }
 
 /**
