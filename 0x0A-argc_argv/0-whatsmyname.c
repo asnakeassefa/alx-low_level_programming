9653ef47b9bc788b0f4fad1b713f3fdaec3a0e9d
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-01 17:38:20 +0300

    added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,11 @@
  * Return: int
  */
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
-	printf(argv[--argc]);
+	if(argc == 1)
+	{
+	printf(argv[argc - 1]);
+	}
 	return (0);
 }
