82014ed6bc24b57296512ffce174196ad57b54a8
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-04 22:01:26 +0300

    added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,49 +4,34 @@
 
 /**
  * str_concat - function
- * 
- * @s1: para 
- * @s2: para2 
- * Return char* 
+ *
+ * @s1: para
+ * @s2: para2
+ * Return: char*
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
 	int i = 0, size2 = 0, size1 = 0;
-	if(s1 == NULL)
-		return(NULL);
-	if(s2 == NULL)
-		return(NULL);
 
-	while(s1[size1])
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+
+	while (s1[size1])
 		size1++;
-	while(s2[size2])
+	while (s2[size2])
 		size2++;
 	arr = malloc((size1 + size2) * sizeof(char) + 1);
-	while(i < (size1 + size2))
+	while (i < (size1 + size2))
 	{
-		if(i < size1)
+		if (i < size1)
 			arr[i] = s1[i];
-		else if(i >= size1)
+		else if (i >= size1)
 			arr[i] = s2[i - size1];
 		i++;
 	}
 	arr[i] = '\0';
-	return(arr);
-}
-
-int main(void)
-{
-    char *s;
-
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	return (arr);
 }
