f31dc1637de286828d411887226aa203f9f9d75f
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-04 21:17:31 +0300

    added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - funct
+ *
+ * @str: para
+ * Return: char*
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int size = 0;
+	int i;
+
+	while (*str++)
+		size++;
+	arr = malloc(size * sizeof(char) + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = str[i];
+	arr[i] = '\0';
+	return (arr);
+}
