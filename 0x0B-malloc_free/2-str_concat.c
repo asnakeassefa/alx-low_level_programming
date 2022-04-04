fe099207ba2eee4c33e64df77db51815aba8283d
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-04 22:00:02 +0300

    added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,26 +13,26 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	int i = 0, size2, size1;
+	int i = 0, size2 = 0, size1 = 0;
 	if(s1 == NULL)
 		return(NULL);
 	if(s2 == NULL)
 		return(NULL);
 
-	while(*s1++)
-	{
+	while(s1[size1])
 		size1++;
-	}
-	while(*s2++)
+	while(s2[size2])
 		size2++;
 	arr = malloc((size1 + size2) * sizeof(char) + 1);
 	while(i < (size1 + size2))
 	{
 		if(i < size1)
 			arr[i] = s1[i];
-		else if(i > size1)
+		else if(i >= size1)
 			arr[i] = s2[i - size1];
+		i++;
 	}
+	arr[i] = '\0';
 	return(arr);
 }
 
