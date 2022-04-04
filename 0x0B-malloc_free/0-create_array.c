28f2ca1ac420686dd97bbef9271a46c3de555601
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-04 19:29:29 +0300

    added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *create_array - funcition
+ *
+ * @size: is para
+ * @c: is para
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	char *arr = malloc(size * sizeof(char));
+	for (i = 0; i < size * sizeof(char); i++)
+		arr[i] = c;
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
+}
