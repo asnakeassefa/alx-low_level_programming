b9fbb2826c373dbe02ac6e6c27696292bbbb953a
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-11 23:03:09 +0300

    structure all

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - structure
+ *
+ * @name: 1st mem
+ * @age: 2nd mem
+ * @owner: 3rd mem
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
