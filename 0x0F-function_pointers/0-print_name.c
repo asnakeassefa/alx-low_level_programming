96f87fbaf0c818128462a7e8b73451e606e700f4
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-13 23:38:25 +0300

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - is function
+ *
+ * @name: is para
+ * @f: is para
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
