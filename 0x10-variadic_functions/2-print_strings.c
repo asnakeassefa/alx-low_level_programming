5c60470493a2dbef21090f6bf43f32d6d671a5b4
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-15 00:08:16 +0300

    updated

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - it print string
+ *
+ * @separator: is para
+ * @n: is para
+ * @...: ispara
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *a;
+	va_list li;
+
+	va_start(li, n);
+
+	for (i = 0; i < n; i++)
+	{
+		a = va_arg(li, char *);
+		if (a == NULL)
+			printf("%s", "(nil)");
+		printf("%s", a);
+		if (separator && i < n - 1)
+			printf("%c ", *separator);
+	}
+	printf("\n");
+	va_end(li);
+}
