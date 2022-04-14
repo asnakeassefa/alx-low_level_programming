2c4c2c00f0c760c70e4277003f0a793319e95767
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-14 23:33:57 +0300

    update added

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - is fun
+ *
+ * @separator: is para
+ * @n: is para
+ * @...: is para
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list li;
+
+	va_start(li, n);
+	if (*separator == '\0')
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(li, int));
+		if (i < n - 1)
+			printf("%c ", *separator);
+	}
+	printf("\n");
+}
