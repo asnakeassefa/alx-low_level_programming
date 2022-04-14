1f93c9c6b29eb25fcc3185f980211174e817fc0e
asnakeassefa <asnakeassefa28@gmail.com>
2022-04-14 14:28:58 +0300

    2-int_index updated

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function
+ *
+ * @array: is para 1
+ * @size: is para 2
+ * @cmp: is para 3
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
